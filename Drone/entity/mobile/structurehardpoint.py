66a16e8b5de5b3a126987dccf93ba10731a1c664
MoyTW <moytravis@gmail.com>
2013-03-14 16:37:44 -0700

    StructureHardpoint is done, I believe. It uses the default module as a 'filler' module to represent the lack of a module. It's, uh, a plug or something, like they put in the barrel of the gun. I dunno.

--- Drone/entity/mobile/structurehardpoint.py
+++ Drone/entity/mobile/structurehardpoint.py
@@ -1,30 +1,51 @@
 __author__ = 'Travis Moy'
 
-import warnings
 from entity.mobile.structureinternal import StructureInternal
+from entity.portable.module.moduletypes import MOD_TYPE
+from entity.portable.module.module import Module
 
 
 class StructureHardpoint(StructureInternal):
 
-    def __init__(self, hardpoint_type, damaged=False, contains=None, fixed=False):
+    # The parent is the Mobile to which this is attached.
+    def __init__(self, hardpoint_type, parent, damaged=False, module=Module(), fixed=False):
         super(StructureHardpoint, self).__init__(damaged)
         self.hardpoint_type = hardpoint_type
-        self.contains = contains
+        self.parent = parent
         self.fixed = fixed
+        self.mount(module)
+
+    def can_mount(self, module):
+        module_type = module.module_type
+        if self.hardpoint_type == module_type:
+            return True
+        elif self.hardpoint_type == MOD_TYPE.weapon_large and (
+                module_type == MOD_TYPE.weapon_medium or
+                module_type == MOD_TYPE.weapon_small):
+            return True
+        elif self.hardpoint_type == MOD_TYPE.weapon_medium and module_type == MOD_TYPE.weapon_small:
+            return True
+        else:
+            return False
 
     def mount(self, module):
-        warnings.warn("StructureHardpoint.mount() is not complete!")
-        self.unmount()
-        self.contains = module
+        if self.can_mount(module):
+            self.unmount()
+            self.module = module
+            self.module.apply_effects(mobile=self.parent, section=self)
+            return True
+        else:
+            return False
 
     def unmount(self):
-        warnings.warn("StructureHardpoint.unmount is not complete!")
+        self.module.remove_effects(mobile=self.parent, section=self)
+        self.module = Module()
 
     def repair(self):
-        pass
+        self.module.repair()
 
     def damage(self):
-        pass
+        self.module.damage()
 
     def destroy(self):
-        pass
+        self.module.destroy()
---
+++ Drone/entity/portable/module/moduletypes.py
@@ -0,0 +1,5 @@
+__author__ = 'Travis Moy'
+
+
+class MOD_TYPE:
+    module, weapon_small, weapon_medium, weapon_large = range(0, 4)
