25ecbb089c3aff6d9cb4fbc732a8209d13f052e0
MoyTW <moytravis@gmail.com>
2013-03-14 20:01:19 -0700

    Moved camera, callbacks into the UIController class.

--- Drone/droneui/uicontroller.py
+++ Drone/droneui/uicontroller.py
@@ -2,15 +2,23 @@ __author__ = 'Travis Moy'
 
 from modeexploration import ModeExploration
 from uimodelist import UIModeList
+import droneui
 
 
 class UIController(object):
 
-    def __init__(self, window, drone, camera):
+    def __init__(self, window, drone, level):
         self.window = window
-        self.mode_list = UIModeList(drone, camera)
+        self.camera = droneui.camera.Camera(level, (0, 0), (640, 480))
+        self.mode_list = UIModeList(drone, self.camera)
         self.key_input_mode = self.mode_list.exploration
 
+        self._setup_callbacks()
+
+    def center_on(self, x, y):
+        self.camera.center_on(x, y)
+
+    def _setup_callbacks(self):
         @self.window.event
         def on_key_press(symbol, modifiers):
             self.key_input_mode = self.key_input_mode.handle_keys(symbol, modifiers)
@@ -22,3 +30,9 @@ class UIController(object):
                 pass
 
             return True
+
+        @self.window.event
+        def on_draw():
+            self.window.clear()
+            self.camera.batch.draw()
+            self.camera.cursor.draw()
--- Drone/main.py
+++ Drone/main.py
@@ -25,18 +25,10 @@ drone = Mobile(droneimage, level)
 level.place_entity_at(drone, 1, 1)
 drone.current_cell = Position(1, 1)
 
-cam = Camera(level, (0, 0), (640, 480))
-cam.center_on(1, 1)
-
 window = pyglet.window.Window()
-controller = droneui.uicontroller.UIController(window, drone, cam)
+controller = droneui.uicontroller.UIController(window, drone, level)
+controller.center_on(1, 1)
 
 look = False
-
-@window.event
-def on_draw():
-    window.clear()
-    cam.batch.draw()
-    cam.cursor.draw()
 
 pyglet.app.run()
